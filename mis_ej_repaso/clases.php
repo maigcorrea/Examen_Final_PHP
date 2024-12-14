6ffd300bd4bd2acf8d6520000e2aceb60c839e09
Maite <anamaitegarciacorrea@gmail.com>
2024-12-14 17:57:40 +0100

    Ejercicios hasta el 4 hechos. Voy a iniciar el 5

--- mis_ej_repaso/clases.php
+++ mis_ej_repaso/clases.php
@@ -24,6 +24,27 @@
         }
 
 
+        //Función para insertar cliente
+        public function insertar_cliente(){
+            $sent="INSERT INTO Cliente VALUES(?,?,?,?,?);";
+
+            //Como es insertar, no nos va a traer datos(fetch) y no hace falta almacenar resultados(bind_result)
+            try{
+                $cons=$this->bd->prepare($sent);
+                $cons->bind_param("ssiss", $this->nif, $this->nombre, $this->edad,$this->usuario, $this->contraseña);
+                $cons->execute();
+
+                echo "<p>Usuario registrado correctamente</p>";
+                $cons->close();
+            }catch(Exception $e){
+                echo "<p>Error al insertar nuevo cliente en la bd</p><br>";
+                echo $e->getMessage();
+            }
+            
+
+        }
+
+
         //Función para obtener todos los datos de los clientes menos la contraseña
         public function obtener_datos(){
             try{
@@ -65,9 +86,75 @@
 
 
     class Venta{
+        private $bd;
+        private $cliente;
         private $producto;
         private $fecha;
         private $cantidad;
         private $estado;
+
+
+        public function __construct(Mysqli $db, $cli="", $p=0, $fech="", $cant=0, $est=null){
+            $this->bd=$db;
+            $this->cliente=$cli;
+            $this->producto=$p;
+            $this->fecha=$fech;
+            $this->cantidad=$cant;
+            $this->estado=$est;
+        }
+
+
+        public function mostrar_ventas(){
+            $sent="SELECT c.nombre, p.descripcion, v.fecha, v.cantidad, v.estado FROM Cliente c,Producto p,Venta v WHERE c.nif=v.cliente AND p.cod=v.producto;";
+
+            $cons=$this->bd->prepare($sent);
+            $cons->bind_result($this->cliente,$this->producto,$this->fecha,$this->cantidad,$this->estado);
+            $cons->execute();
+
+            //Nos va a devolver más de una fila para cada dato, hay que hacer un bucle
+            while($cons->fetch()){
+                echo $this;
+            }
+
+            $cons->close();
+        }
+
+
+
+        public function __toString(){
+            $str="Cliente: $this->cliente<br>Producto: $this->producto<br>Fecha de compra: $this->fecha<br>Cantidad: $this->cantidad<br>Estado: $this->estado<br><br><br>";
+
+            return $str;
+        }
+    }
+
+
+
+    class Producto{
+        private $bd;
+        // private $cod;
+        private $descripcion;
+        private $precio;
+
+        public function __construct(Mysqli $db, $desc="", $p=0){
+            $this->bd=$db;
+            $this->descripcion=$desc;
+            $this->precio=$p;
+        }
+
+        public function insertar_Producto(){
+            $sent="INSERT INTO producto(descripcion,precio) VALUES(?,?);";
+
+            try {
+                $cons=$this->bd->prepare($sent);
+                $cons->bind_param("sd",$this->descripcion, $this->precio);
+                $cons->execute();
+
+                $cons->close();
+            } catch (Exception $th) {
+                echo "<p>Error al insertar el producto</p>";
+                echo $th->getMessage();
+            }
+        }
     }
 ?>
--- mis_ej_repaso/ej1.php
+++ mis_ej_repaso/ej1.php
@@ -25,11 +25,72 @@
 
 
         //EJERCICIO 1
+        echo "<h2>EJERCICIO 1</h2>";
         $cliente=new Cliente($bd);
         $cliente->obtener_datos();
         
         //EJERCICIO 2
+        echo "<h2>EJERCICIO 2</h2>";
+        echo "<h3>Las ventas son:</h3>";
+        $ventas=new Venta($bd);
+        $ventas->mostrar_ventas();
+
+        //EJERCICIO 3
+        echo "<h2>EJERCICIO 3 - REGISTRO DE CLIENTE</h2>";
+        //Formulario de registro de cliente
+        if(isset($_POST["reg"])){
+            if($_POST["pass"]===$_POST["repPass"]){
+
+                $regCliente=new Cliente($bd,$_POST["id"], $_POST["name"], $_POST["edad"], $_POST["user"], $_POST["pass"]);
+
+                $regCliente->insertar_cliente();
+                
+            }else{
+                echo "<p>Error. Las contraseñas no coinciden</p>";
+            }
+        }else{
+    ?>
+        <form action="" method="post" enctype="multipart/form-data">
+            <label for="id">Introduce tu nif</label><br>
+            <input type="text" name="id"><br>
+            <label for="name">Introduce tu nombre</label><br>
+            <input type="text" name="name" id=""><br>
+            <label for="edad">Introduce tu edad</label><br>
+            <input type="number" name="edad" id=""><br>
+            <label for="user">Introduce tu nickname</label><br>
+            <input type="text" name="user" id=""><br>
+            <label for="pass">Introduce una contraseña</label><br>
+            <input type="text" name="pass" id=""><br>
+            <label for="repPass">Repite la contraseña</label><br>
+            <input type="text" name="repPass" id=""><br>
+
+            <input type="submit" value="Registrarse" name="reg">
+        </form>
+
+    <?php
+        }
         
+        
+        //EJERCICIO 4
+        echo "<h2>EJERCICIO 4 - REGISTRO DE PRODUCTOS</h2>";
+        if(isset($_POST["regProd"])){
+            $regProd=new Producto($bd,$_POST["desc"],$_POST["prec"]);
+
+            $regProd->insertar_Producto();
+        }else{
+    ?>
+
+        <form action="#" method="post" enctype="multipart/form-data">
+            <label for="desc">Introduce el nombre del producto</label><br>
+            <input type="text" name="desc" id=""><br>
+            <label for="prec">Introduce su precio</label><br>
+            <input type="number" name="prec" step="0.11" id=""><br>
+
+            <input type="submit" value="Registrar producto" name="regProd">
+        </form>
+
+    <?php
+        }
     ?>
 </body>
 </html>
