65a768f958b307bde0791516032c578d88a2a383
Maite <anamaitegarciacorrea@gmail.com>
2024-12-15 19:57:55 +0100

    Hago el ej 3

--- mis_ej_repaso/ejDeNuevo/clases2.php
+++ mis_ej_repaso/ejDeNuevo/clases2.php
@@ -16,6 +16,17 @@
             $this->pass=$p;
         }
 
+        public function insertarCliente(){
+            $sent="INSERT INTO cliente VALUES(?,?,?,?,?);";
+
+            $cons=$this->bd->prepare($sent);
+            $cons->bind_param("ssiss",$this->nif, $this->nombre, $this->edad, $this->usuario, $this->pass);
+            $cons->execute();
+
+            $cons->close();
+        }
+
+
         public function mostrarDatos(){
             $sent="SELECT nif,nombre,edad,usuario FROM cliente";
 
@@ -26,10 +37,55 @@
             while($cons->fetch()){
                 echo $this;
             }
+
+            $cons->close();
         }
+
 
         public function __toString(){
             $str="<br>NIF: $this->nif<br>NOMBRE: $this->nombre<br>EDAD: $this->edad<br> USUARIO: $this->usuario<br>";
+            return $str;
+        }
+    }
+
+
+    class Venta{
+        private $bd;
+        private $cliente;
+        private $producto;
+        private $fecha;
+        private $cantidad;
+        private $estado;
+
+
+        public function __construct(MySqli $db, $cli="",$prod=0, $fe="",$cant="", $est=null){
+            $this->bd=$db;
+            $this->cliente=$cli;
+            $this->producto=$prod;
+            $this->fecha=$fe;
+            $this->cantidad=$cant;
+            $this->estado=$est;
+        }
+
+
+        public function mostrarVentas(){
+            $sent="SELECT c.nombre, p.descripcion, v.fecha, v.cantidad, v.estado FROM venta v ,cliente c, producto p WHERE v.cliente=c.nif AND v.producto=p.cod;";
+
+            $cons=$this->bd->prepare($sent);
+            $cons->bind_result($this->cliente, $this->producto, $this->fecha, $this->cantidad, $this->estado);
+
+            $cons->execute();
+            while($cons->fetch()){
+                echo $this;
+            }
+
+            $cons->close();
+        }
+
+
+        public function __toString(){
+            $str="Cliente: $this->cliente<br>Producto: $this->producto<br>Fecha de compra: $this->fecha<br>Cantidad: $this->cantidad<br>Estado: $this->estado<br><br>";
+
             return $str;
         }
     }
--- mis_ej_repaso/ejDeNuevo/ej2.php
+++ mis_ej_repaso/ejDeNuevo/ej2.php
@@ -20,6 +20,43 @@
         echo "<h2>EJERCICIO 1 - MOSTRAR DATOS DE CLIENTES</h2>";
         $cli=new Cliente($bd);
         $cli->mostrarDatos();
+
+
+        
+        //EJERCICIO 2
+        echo "<h2>EJERCICIO 2 - MOSTRAR VENTAS</h2>";
+        $venta=new Venta($bd);
+        $venta->mostrarVentas();
+
+
+
+        //EJERCICIO 3
+        echo "<h2>EJERCICIO 3 - INSERTAR CLIENTE</h2>";
+        //En base a los datos de un formulario, creamos un nuevo cliente, y llamamos a la funciín de insertarCliente
+
+        if(isset($_POST["regCli"])){
+            if($_POST["cont1"]===$_POST["cont2"]){
+                $cliente=new Cliente($bd,$_POST["nif"],$_POST["nom"],$_POST["ed"],$_POST["user"],$_POST["cont1"]);
+
+                $cliente->insertarCliente();
+            }else{
+                echo "<p>Las contraseñas no coinciden</p>";
+            }
+        }else{
+    ?>
+        <form action="#" method="post" enctype="multipart/form-data">
+            <input type="text" name="nif" id="" placeholder="NIF"><br>
+            <input type="text" name="nom" id="" placeholder="Nombre"><br>
+            <input type="number" name="ed" id="" placeholder="Edad"><br>
+            <input type="text" name="user" id="" placeholder="Usuario"><br>
+            <input type="text" name="cont1" id="" placeholder="Contraseña"><br>
+            <input type="text" name="cont2" id="" placeholder="Repetir Contraseña"><br>
+
+            <input type="submit" value="Registrar usuario" name="regCli">
+        </form>
+
+    <?php
+        }
     ?>
 </body>
 </html>
